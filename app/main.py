dbc2c05a86987737a096219e38165f0021700807
Orest Yakymiv <akimivorest@gmail.com>
2025-03-18 14:31:56 +0200

    get data from cache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    memory = {}
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args not in memory:
+            memory[args] = func(*args, **kwargs) # (1,2,3): (1,2,3)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return memory[args]
+    return wrapper
+
+
+
+
+
+def getting_from_cache(*args):
+    return args
